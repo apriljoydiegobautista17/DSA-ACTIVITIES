4218fca247520f3bc52234992f952285ceccfc31
apriljoydiegobautista17 <146685369+apriljoydiegobautista17@users.noreply.github.com>
2023-10-14 14:38:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//ACTIVITY NUMBER8
+
+let favSingers = ['Tylor Swift', 'Sarah G.', 'TJ Monterde'];
+console.log(favSingers);
+
+let favNumbers = ['17', '19', '4', '1'];
+console.log(favNumbers);
+
+let mixedArr = ["String", ["otherArray"], 123, true];
+
+console.log(favSingers[0]);
+console.log(favSingers[1]);
+console.log(favSingers[2]);
+console.log(favNumbers[1]);
+console.log(mixedArr[3]);
